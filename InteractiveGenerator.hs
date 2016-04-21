7c409a3b264be8f3849420a16deebad22bb9cd8d
Shane Peelar <lookatyouhacker@gmail.com>
2016-04-21 19:41:12 -0400

    Initial interactive.hs generation for interactive sessions

---
+++ InteractiveGenerator.hs
@@ -0,0 +1,39 @@
+import qualified SolarmanTriplestore as App
+import qualified TypeAg2 as TypeAg
+import qualified Data.Map.Strict as Map
+import System.IO
+import Data.String.Utils
+import Data.List
+import Data.Char
+
+{-Objective: generate the file Interactive.hs with variables corresponding to things in the database,
+ - e.g. variable hall = make_proper "hall "-}
+
+filterDict t = filter (\(_,y,_) -> y == t) App.dictionary
+
+makeFriendlyName = map toLower . replace "-" "_"
+
+v_make_pnoun name = makeFriendlyName name ++ " = make_pnoun \"" ++ name ++ "\""
+v_make_cnoun name = makeFriendlyName name ++ " = get_members dataStore \"" ++ name ++ "\""
+
+
+typeActionMap = Map.fromList [(TypeAg.Pnoun, v_make_pnoun),(TypeAg.Cnoun, v_make_cnoun)]
+
+
+genVariables = foldr var [] App.dictionary
+    where
+    var (name,memoL,_) list | Just f <- Map.lookup memoL typeActionMap = (f name):list
+    var (name,memoL,_) list = list
+
+
+printVars handle = mapM_ (hPutStrLn handle) (nub genVariables)
+
+
+main = do
+    file <- openFile "Interactive.hs" WriteMode
+    hPutStrLn file "import SolarmanTriplestore as App"
+    hPutStrLn file "import Getts as Getts"
+    hPutStrLn file ""
+    printVars file
+    hClose file
+
