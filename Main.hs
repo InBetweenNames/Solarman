d73fc0932dbe7ae646417acccbddf2910b8d1bdb
Shane Peelar <lookatyouhacker@gmail.com>
2017-08-23 07:47:50 -0400

    UTF-8 support in Main.hs

--- Main.hs
+++ Main.hs
@@ -1,47 +1,52 @@
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE DoAndIfThenElse #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 import qualified XSaiga.SolarmanTriplestore as App
 import Network.CGI
 import qualified Data.List as List
+import Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.ByteString.Lazy as BL
+import Data.Text.Encoding as E
 
 cgiMain :: CGI CGIResult
 cgiMain = do
-    (Just input) <- getInput "query"
-    out <- liftIO $ interpret' input
-    output out
+    (Just input) <- getInputFPS "query"
+    out <- liftIO $ interpret' $ E.decodeUtf8 $ BL.toStrict input
+    setHeader "Content-type" "text/plain; charset=utf-8"
+    outputFPS $ BL.fromStrict $ E.encodeUtf8 $ out
 
 
 main :: IO ()
 main = runCGI (handleErrors cgiMain)
 
-interpret :: String -> String
 interpret "ask them to be quiet" 
      = "Hello. Quiet please. My "
-       ++ "masters are going to talk. Quiet please."
+       `T.append` "masters are going to talk. Quiet please."
 
 interpret "introduce yourself solar man" 
      =  "Hello. Hello. My name is Solar man."
-        ++ " Thank you for coming to my party."
-        ++ " I am very pleased to meet you."
+        `T.append` " Thank you for coming to my party."
+        `T.append` " I am very pleased to meet you."
 
 interpret "what can i say" 
       = "You can say. hello there. what is your name."
-        ++ " you can ask me about the moons and the planets."
-    ++ " such as, who discovered a moon."
-    ++ " who discovered two moons."
-    ++ " which moons were discovered by kuiper."
-    ++ " who discovered phobos."
-    ++ " which planet is orbited by miranda."
-    ++ " how many moons orbit saturn."
-    ++ " and other similar questions."
-        ++ " who are you. where do you live."
-        ++ " tell me a joke. who made you." 
-        ++ " who do you know. what is your favorite band."
-        ++ " who is the vice president at the university of windsor." 
-        ++ " who is the president at the university of windsor." 
-        ++ " who is the dean of science at the university of windsor."
+        `T.append` " you can ask me about the moons and the planets."
+    `T.append` " such as, who discovered a moon."
+    `T.append` " who discovered two moons."
+    `T.append` " which moons were discovered by kuiper."
+    `T.append` " who discovered phobos."
+    `T.append` " which planet is orbited by miranda."
+    `T.append` " how many moons orbit saturn."
+    `T.append` " and other similar questions."
+        `T.append` " who are you. where do you live."
+        `T.append` " tell me a joke. who made you." 
+        `T.append` " who do you know. what is your favorite band."
+        `T.append` " who is the vice president at the university of windsor." 
+        `T.append` " who is the president at the university of windsor." 
+        `T.append` " who is the dean of science at the university of windsor."
 
 interpret "hi"              = "Hi there. My name is solar man"
 interpret "hello"           = "hello. My name is solar man."
@@ -56,20 +61,20 @@ interpret "yes please" = "yes please? What did you want? My memory is getting ba
 interpret "what is your name" = "My name is solar man."
 interpret "who are you" 
    = "My name is solar man. I know about the planets and the"
-     ++ " moons, and the people who discovered them"
+     `T.append` " moons, and the people who discovered them"
 interpret "where do you live" 
     = "I live in a dark cold computer. "
-       ++ "The center of my universe is Lambton Tower, at the University of Windsor."
+       `T.append` "The center of my universe is Lambton Tower, at the University of Windsor."
 interpret "what do you know" 
    = "Not much I am afraid. I am just beginning to learn. I know a bit about "
-     ++ "the planets, the moons, and the people who discovered them. "
-     ++ "My master will teach me some more when he gets another grant "
+     `T.append` "the planets, the moons, and the people who discovered them. "
+     `T.append` "My master will teach me some more when he gets another grant "
 interpret "how old are you"
   = "older than you think. And much older than my friends Judy and Monty."
 interpret "who made you" 
    = "I. B. M. and Opera Software made my ears and vocal chords. William Ma connected my "
-     ++ "ears to my brain, and Doctor Frost, master of the universe, made "
-     ++ "my brain"
+     `T.append` "ears to my brain, and Doctor Frost, master of the universe, made "
+     `T.append` "my brain"
 
 interpret "what is your favorite band"
    = "Pink Floyd. I love, dark side of the moon"
@@ -91,7 +96,7 @@ interpret "know any poems" = "no but my friend, Judy does;"
 
 interpret "tell me a joke" 
     = "did you hear about the Computer Scientist who thought his computer"
-      ++ "was a car. He had a hard drive home every day"
+      `T.append` "was a car. He had a hard drive home every day"
 
 interpret "know any jokes" 
        = "just one. My friend Monty knows one too."
@@ -100,18 +105,18 @@ interpret "who is judy" = "She is my friend. She knows about poetry"
 
 interpret "who is monty"   
    = "Monty is my friend. He is a student"
-     ++ " at the university of Windsor."
+     `T.append` " at the university of Windsor."
 interpret "can I talk to judy" 
               ="yes. here she is"
-                ++ "<goto>http://cs.uwindsor.ca/~speechweb/p_d_speechweb/judy/judy.xml</goto>"
+                `T.append` "<goto>http://cs.uwindsor.ca/~speechweb/p_d_speechweb/judy/judy.xml</goto>"
 
 interpret "can I talk to monty" 
               ="yes. here he is"
-                ++ "<goto>http://cs.uwindsor.ca/~speechweb/p_d_speechweb/monty/monty.xml</goto>"
+                `T.append` "<goto>http://cs.uwindsor.ca/~speechweb/p_d_speechweb/monty/monty.xml</goto>"
 
 interpret "can I talk to solar man" 
               ="yes. here he is"
-                ++ "<goto>http://cs.uwindsor.ca/~speechweb/p_d_speechweb/solarman/solarman.xml</goto>"
+                `T.append` "<goto>http://cs.uwindsor.ca/~speechweb/p_d_speechweb/solarman/solarman.xml</goto>"
 
 interpret "who do you know" = "i only know three people. Judy, Monty, and Solarman."
 
@@ -121,8 +126,8 @@ interpret' input = do
     let firstpass = interpret input
     if firstpass == "BLANKVALNOTUSED" then do
         output <- App.formatParseIO input
-        let formatted = concat $ List.intersperse " " output
-        if null formatted then return "Do not know that one yet, will work on it tonight" else return $ formatted
+        let formatted = T.concat $ List.intersperse " " output
+        if T.null formatted then return "Do not know that one yet, will work on it tonight" else return $ formatted
     else return firstpass
 
 
