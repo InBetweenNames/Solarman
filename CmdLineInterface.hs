9f031950716aa42a78fa17ce32a1ea6295dd6b1d
Shane Peelar <lookatyouhacker@gmail.com>
2017-08-23 18:09:44 -0400

    Update command line interface

--- CmdLineInterface.hs
+++ CmdLineInterface.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CmdLineInterface where
 
 import XSaiga.SolarmanTriplestore
 import System.Environment
 import Data.List
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 main = do
   [query] <- getArgs
-  res <- formatParseIO query
-  putStrLn $ concat $ intersperse " " res
+  res <- formatParseIO $ T.pack query
+  TIO.putStrLn $ T.concat $ intersperse " " res
 
